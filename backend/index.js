802003d71f65b7f41e765ab414112dad0641bafb
Alaamuhammeddd <alaamuhammeddd17@gmail.com>
2023-04-12 22:40:51 +0200

    Working Bid on auction with search w mafesh errors ya ahmed ya allam

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const app = express();
 const register = require('./routs/auth/registration');
 const login = require('./routs/auth/login');
 const logout = require('./routs/auth/logout');
+const bidOnAuction = require('./routs/bidOnAuction/bidOnDesiredAuction');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
@@ -14,6 +15,7 @@ app.use(express.static(path.join(__dirname,"")));
 app.use("/login",login);
 app.use('/logout',logout);
 app.use('/registration',register);
+app.use('/auctions',bidOnAuction);
 app.listen(4000,"localhost",()=>{
 
     console.log("server is running");
